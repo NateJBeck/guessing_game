684bd05cf0109d39542dcf3aaa77b931b2e10f6a
Nathan Beck <nathan.beck10@gmail.com>
2014-10-20 17:23:48 -0400

    Guess about each image and keep correct-answer tally

--- guessing_game_jquery.js
+++ guessing_game_jquery.js
@@ -1,26 +1,50 @@
 var images = [
-  "http://1.bp.blogspot.com/-PCjKh0ldpUU/UrMzi3uMOpI/AAAAAAAACjI/2VxOmXdhTSY/s1600/8097974222_043436e208_o.jpg",
-  "http://www.petsandparasites.org/images/uploads/images/Mixed_Breed_Puppy_Four_Months.jpg"
+{url:"http://1.bp.blogspot.com/-PCjKh0ldpUU/UrMzi3uMOpI/AAAAAAAACjI/2VxOmXdhTSY/s1600/8097974222_043436e208_o.jpg", type:"dog"},
+{url:"http://www.petsandparasites.org/images/uploads/images/Mixed_Breed_Puppy_Four_Months.jpg", type:"dog"},
+{url:"http://upload.wikimedia.org/wikipedia/commons/2/22/Turkish_Van_Cat.jpg", type:"cat"},
+{url:"http://i4.mirror.co.uk/incoming/article2106793.ece/alternates/s2197/Fluffy-White-dog.jpg", type:"dog"}
 ];
-index = 0;
+
+var index = 0;
+var correctAnswers = 0;
 
 $(function(){
-  $("#dog").click(sayYes);
-  $("#cat").click(sayNo);
-  $("#next").click(nextImage);
+  populateFirstImage();
+  $("#dog").click(dogYes);
+  $("#cat").click(catYes);
 });
 
-function sayYes() {
-  $("#result").text("Yes")
+var populateFirstImage = function() {
+  $("#image").attr("src", images[index].url);
+}
+
+var dogYes = function() {
+  if ( images[index].type === "dog" ){
+    correctAnswers++;
+    $("#counter").text("Correct Answers: " + correctAnswers);
+    nextImage();
+  } else {
+    //$("#result").text("No");
+    nextImage();
+  }
   //get a result object HTML page, calls .text method to change the text
+  //("#result").text("Yes");
 };
 
-function sayNo() {
-  $("#result").text("No")
+var catYes = function() {
+  if ( images[index].type === "cat" ){
+   // $("#result").text("Yes");
+    correctAnswers++;
+    nextImage();
+  } else if ( images[index].type === "dog" ){
+   // $("#result").text("No");
+    nextImage();
+  }
 };
 
-function nextImage() {
-  $("#image").attr("src", images[index]);
-  index++;
+var nextImage = function() {
+  if( (index+1) < images.length ){
+    index++;
+    $("#image").attr("src", images[index].url);
+  }
 };
-
