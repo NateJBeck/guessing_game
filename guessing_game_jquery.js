855794de097b80ba2553203545588cc6419bb9f2
Nathan Beck <nathan.beck10@gmail.com>
2014-10-20 17:29:10 -0400

    Fix tallying for correct cat answers

--- guessing_game_jquery.js
+++ guessing_game_jquery.js
@@ -1,8 +1,8 @@
 var images = [
-{url:"http://1.bp.blogspot.com/-PCjKh0ldpUU/UrMzi3uMOpI/AAAAAAAACjI/2VxOmXdhTSY/s1600/8097974222_043436e208_o.jpg", type:"dog"},
-{url:"http://www.petsandparasites.org/images/uploads/images/Mixed_Breed_Puppy_Four_Months.jpg", type:"dog"},
-{url:"http://upload.wikimedia.org/wikipedia/commons/2/22/Turkish_Van_Cat.jpg", type:"cat"},
-{url:"http://i4.mirror.co.uk/incoming/article2106793.ece/alternates/s2197/Fluffy-White-dog.jpg", type:"dog"}
+  {url:"http://1.bp.blogspot.com/-PCjKh0ldpUU/UrMzi3uMOpI/AAAAAAAACjI/2VxOmXdhTSY/s1600/8097974222_043436e208_o.jpg", type:"dog"},
+  {url:"http://www.petsandparasites.org/images/uploads/images/Mixed_Breed_Puppy_Four_Months.jpg", type:"dog"},
+  {url:"http://upload.wikimedia.org/wikipedia/commons/2/22/Turkish_Van_Cat.jpg", type:"cat"},
+  {url:"http://i4.mirror.co.uk/incoming/article2106793.ece/alternates/s2197/Fluffy-White-dog.jpg", type:"dog"}
 ];
 
 var index = 0;
@@ -34,6 +34,7 @@ var dogYes = function() {
 var catYes = function() {
   if ( images[index].type === "cat" ){
     correctAnswers++;
+    $("#counter").text("Correct Answers: " + correctAnswers);
     nextImage();
   } else if ( images[index].type === "dog" ){
     nextImage();
